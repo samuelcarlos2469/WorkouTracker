63a3e803b18191845f668c284bf64a7a3f01b7a4
samuelcarlos2469 <samuelcarlos701estudos@gmail.com>
2024-12-17 15:48:54 -0400

    expandir instruções com useState

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -1,17 +1,23 @@
-import { useLocalSearchParams } from "expo-router";
 import exercises from "../../assets/data/exercises.json";
+import { useLocalSearchParams } from "expo-router";
 import { Stack } from "expo-router";
-import { ScrollView } from "react-native";
+import { ScrollView, StyleSheet , View, Text} from "react-native";
+import { useState } from "react";
+
 export default function ExerciseDetailsScreen() {
   const params = useLocalSearchParams();
+
+  const [isInstructionExpanded, setisInstructionExpanded] = useState(false);
+
   const exercise = exercises.find((item) => item.name === params.name);
+
   if (!exercise) {
     return <Text>Exercise not found</Text>;
   }
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Stack.Screen options={{title: exercise.name}} />
-      
+
       <View styles={styles.panel}></View>
         <Text style={styles.exerciseName}>{exercise.name}</Text>
         <Text style={styles.exerciseSubtitle}>
@@ -20,7 +26,12 @@ export default function ExerciseDetailsScreen() {
         </Text>
 
       <View>
-        <Text style={styles.instructions}>{exercise.instructions}</Text>
+        <Text style={styles.instructions} numberOfLines={isInstructionExpanded ? 0 : 3}>
+          {exercise.instructions}
+          </Text>
+        <Text style={styles.seeMore} onPress={() => setisInstructionExpanded(!isInstructionExpanded)}>
+          {isInstructionExpanded ? "See less" : "See more"}
+          </Text>
       </View>
     </ScrollView>
   );
@@ -41,5 +52,11 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 5
+  },
+  seeMore:{
+    alignSelf: "center",
+    padding: 5,
+    fontWeight: "600",
+    color:"gray"
   }
 });
--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,5 +1,5 @@
 import { Stack } from "expo-router";
-export default function RootLayou(){
+export default function RootLayout(){
     return (
     <Stack>
         <Stack.Screen name="index" options={{title: "Exercises"}}/>
