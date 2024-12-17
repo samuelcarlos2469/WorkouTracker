4dd8049eee32d79991059756c393ed3e283fbb42
samuelcarlos2469 <samuelcarlos701estudos@gmail.com>
2024-12-17 15:35:50 -0400

    criando layout e começando o navigation

---
+++ src/app/[name].jsx
@@ -0,0 +1,45 @@
+import { useLocalSearchParams } from "expo-router";
+import exercises from "../../assets/data/exercises.json";
+import { Stack } from "expo-router";
+import { ScrollView } from "react-native";
+export default function ExerciseDetailsScreen() {
+  const params = useLocalSearchParams();
+  const exercise = exercises.find((item) => item.name === params.name);
+  if (!exercise) {
+    return <Text>Exercise not found</Text>;
+  }
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Stack.Screen options={{title: exercise.name}} />
+      
+      <View styles={styles.panel}></View>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
+        <Text style={styles.exerciseSubtitle}>
+          <Text style={styles.subValue}>{exercise.muscle}</Text> |{" "}
+          <Text style={styles.subValue}>{exercise.equipment}</Text>
+        </Text>
+
+      <View>
+        <Text style={styles.instructions}>{exercise.instructions}</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { padding: 10, gap: 10},
+  exerciseName: { fontSize: 20, fontWeight: "500" },
+  exerciseSubtitle: { color: "dimgray" },
+  subValue: {
+    textTransform: "capitalize",
+  },
+  instructions:{
+    fontSize: 16,
+    lineHeight:22
+  },
+  panel:{
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5
+  }
+});
---
+++ src/app/_layout.jsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+export default function RootLayou(){
+    return (
+    <Stack>
+        <Stack.Screen name="index" options={{title: "Exercises"}}/>
+    </Stack>
+    )
+}
