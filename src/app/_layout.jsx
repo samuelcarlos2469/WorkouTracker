448f8286356f268d74ba079f5a495bc8187a3e9b
samuelcarlos2469 <samuelcarlos701estudos@gmail.com>
2025-05-04 17:37:50 -0400

    sync

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,8 +1,14 @@
 import { Stack } from "expo-router";
-export default function RootLayout(){
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const client = new QueryClient();
+
+export default function RootLayout() {
     return (
-    <Stack>
-        <Stack.Screen name="index" options={{title: "Exercises"}}/>
-    </Stack>
+        <QueryClientProvider client={client}>
+            <Stack>
+                <Stack.Screen name="index" options={{ title: "Exercises" }} />
+            </Stack>
+        </QueryClientProvider>
     );
 }
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,13 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from "react-native";
 import exercises from "../../assets/data/exercises.json";
 import ExerciseListItem from "../../src/components/ExerciseListItem";
+import { useQuery } from "@tanstack/react-query";
+import { gql, request } from 'graphql-request';
 
-export default function App() {
+const url = 'https://shagoutang.us-east-a.ibm.stepzen.net/api/wishing-dog/graphql';
+const exercisesQuery = gql`
+query exercises($muscle: String, $name: String) {
+  exercises(muscle: $muscle, name: $name) {
+    name
+    muscle
+  }
+}
+`;
+
+export default function ExercisesScreen() {
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['exercises'],
+    queryFn: async () => {
+      return request({
+        url, document: exercisesQuery, requestHeaders: {
+          "Authorization": "apikey shagoutang::local.net+1000::00dc84ac4c87d268ddec0d2d96d46258f55e20633812d5cce13da336da328836"
+        }
+      });
+    },
+
+  })
+  if (isLoading) {
+    return <ActivityIndicator />
+  }
+
+  // if (error)
+  //   return <Text>Failed to fecth exercises{error.message}</Text>
   return (
     <View style={styles.container}>
       <FlatList
-        data={exercises}
+        data={data?.exercises}
         contentContainerStyle={{ gap: 5 }}
         keyExtractor={(item) => item.name}
         renderItem={({ item }) => <ExerciseListItem item={item} />}
